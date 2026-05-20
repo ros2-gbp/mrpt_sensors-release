3a8cd06d94c1922c079c4180346760342353e6f6
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-05-20 13:31:03 +0200

    Rebase from 'upstream'

--- mrpt_sensor_imu_taobotics/src/mrpt_sensor_imu_taobotics_node.cpp
+++ mrpt_sensor_imu_taobotics/src/mrpt_sensor_imu_taobotics_node.cpp
@@ -1,7 +1,7 @@
 /* +------------------------------------------------------------------------+
    |                             mrpt_sensors                               |
    |                                                                        |
-   | Copyright (c) 2017-2024, Individual contributors, see commit authors   |
+   | Copyright (c) 2017-2026, Individual contributors, see commit authors   |
    | See: https://github.com/mrpt-ros-pkg/mrpt_sensors                      |
    |                                                                        |
    | Redistribution and use in source and binary forms, with or without     |
@@ -32,6 +32,7 @@
    | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   |
    |                                                                        |
    | All rights reserved. Released under BSD 3-Clause license. See LICENSE  |
+   | SPDX-License-Identifier: BSD-3-Clause                                  |
    +------------------------------------------------------------------------+ */
 
 #include <mrpt/core/bits_math.h>  // square()
@@ -68,41 +69,39 @@ pose_roll    = ${SENSOR_POSE_ROLL}
 
 int main(int argc, char** argv)
 {
-    try
-    {
-        // Init ROS:
-        rclcpp::init(argc, argv);
+  try
+  {
+    // Init ROS:
+    rclcpp::init(argc, argv);
 
-        auto node =
-            std::make_shared<mrpt_sensors::GenericSensorNode>(node_name);
+    auto node = std::make_shared<mrpt_sensors::GenericSensorNode>(node_name);
 
-        node->init(
-            sensorConfig,
-            {
-                {"process_rate", "PROCESS_RATE", "500", false},
-                {"sensor_label", "SENSOR_LABEL", "imu", false},
-                {"serial_port", "SERIAL_PORT", "", true},
-                {"sensor_model", "SENSOR_MODEL", "hfi-a9", false},
+    node->init(
+        sensorConfig,
+        {
+            {"process_rate", "PROCESS_RATE", "500", false},
+            {"sensor_label", "SENSOR_LABEL", "imu", false},
+            {"serial_port", "SERIAL_PORT", "", true},
+            {"sensor_model", "SENSOR_MODEL", "hfi-a9", false},
 
-                {"sensor_pose_x", "SENSOR_POSE_X", "0", false},
-                {"sensor_pose_y", "SENSOR_POSE_Y", "0", false},
-                {"sensor_pose_z", "SENSOR_POSE_Z", "0", false},
-                {"sensor_pose_yaw", "SENSOR_POSE_YAW", "0", false},
-                {"sensor_pose_pitch", "SENSOR_POSE_PITCH", "0", false},
-                {"sensor_pose_roll", "SENSOR_POSE_ROLL", "0", false},
-            },
-            {"SENSOR"});
+            {"sensor_pose_x", "SENSOR_POSE_X", "0", false},
+            {"sensor_pose_y", "SENSOR_POSE_Y", "0", false},
+            {"sensor_pose_z", "SENSOR_POSE_Z", "0", false},
+            {"sensor_pose_yaw", "SENSOR_POSE_YAW", "0", false},
+            {"sensor_pose_pitch", "SENSOR_POSE_PITCH", "0", false},
+            {"sensor_pose_roll", "SENSOR_POSE_ROLL", "0", false},
+        },
+        {"SENSOR"});
 
-        node->run();
+    node->run();
 
-        rclcpp::shutdown();
-        return 0;
-    }
-    catch (const std::exception& e)
-    {
-        RCLCPP_ERROR_STREAM(
-            rclcpp::get_logger(""),
-            "Exception in " << node_name << " main(): " << e.what());
-        return 1;
-    }
+    rclcpp::shutdown();
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger(""), "Exception in " << node_name << " main(): " << e.what());
+    return 1;
+  }
 }

3a5050a32bfb63f93c383959f4de7a1523666e37
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2026-05-20 13:42:20 +0200

    Rebase from 'upstream'

--- mrpt_generic_sensor/src/mrpt_generic_sensor_node.cpp
+++ mrpt_generic_sensor/src/mrpt_generic_sensor_node.cpp
@@ -1,7 +1,7 @@
 /* +------------------------------------------------------------------------+
    |                             mrpt_sensors                               |
    |                                                                        |
-   | Copyright (c) 2017-2024, Individual contributors, see commit authors   |
+   | Copyright (c) 2017-2026, Individual contributors, see commit authors   |
    | See: https://github.com/mrpt-ros-pkg/mrpt_sensors                      |
    |                                                                        |
    | Redistribution and use in source and binary forms, with or without     |
@@ -32,6 +32,7 @@
    | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.   |
    |                                                                        |
    | All rights reserved. Released under BSD 3-Clause license. See LICENSE  |
+   | SPDX-License-Identifier: BSD-3-Clause                                  |
    +------------------------------------------------------------------------+ */
 
 #include <rclcpp/rclcpp.hpp>
@@ -40,26 +41,24 @@
 
 int main(int argc, char** argv)
 {
-    try
-    {
-        // Init ROS:
-        rclcpp::init(argc, argv);
+  try
+  {
+    // Init ROS:
+    rclcpp::init(argc, argv);
 
-        auto node = std::make_shared<mrpt_sensors::GenericSensorNode>();
-        RCLCPP_INFO(
-            node->get_logger(), "About to init MrptGenericSensorNode...");
-        node->init();
-        RCLCPP_INFO(node->get_logger(), "Calling MrptGenericSensorNode::run()");
-        node->run();
+    auto node = std::make_shared<mrpt_sensors::GenericSensorNode>();
+    RCLCPP_INFO(node->get_logger(), "About to init MrptGenericSensorNode...");
+    node->init();
+    RCLCPP_INFO(node->get_logger(), "Calling MrptGenericSensorNode::run()");
+    node->run();
 
-        rclcpp::shutdown();
-        return 0;
-    }
-    catch (const std::exception& e)
-    {
-        RCLCPP_ERROR_STREAM(
-            rclcpp::get_logger(""),
-            "Exception in mrpt_generic_sensor main(): " << e.what());
-        return 1;
-    }
+    rclcpp::shutdown();
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger(""), "Exception in mrpt_generic_sensor main(): " << e.what());
+    return 1;
+  }
 }
